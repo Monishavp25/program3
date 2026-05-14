afbc51ab747c12b695f192a8b2710c2fb12102fa
Monishavp25 <vvce23cseaiml0099@vvce.ac.in>
2026-05-14 21:41:44 +0530

    Replace Java program with Python and Docker setup
    Removed Java code and added Python script along with Docker instructions.

--- First.java
+++ First.java
@@ -1,6 +1,18 @@
+print("Hello world") --->Sample.py
+    
+FROM python
+WORKDIR /app
+COPY . /app
+CMD ["python","Sample.py]  -->dockerfile
 
-public class First{
-    public static void main(String args[]){
-        System.out.println("Hello World");
-    }
-}
+python sample.py
+docker build -t program .
+docker run --name cont1 program
+docker run --name cont2 program
+docker images
+docker image tag "   " monishavp25/program
+docker push monishavp25/program:latest
+    
+
+
+    
