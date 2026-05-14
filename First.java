8afeb3e8ac78b102b42eeca8600734a48da12ad6
Monishavp25 <vvce23cseaiml0099@vvce.ac.in>
2026-05-14 22:05:00 +0530

    Update First.java

--- First.java
+++ First.java
@@ -12,7 +12,67 @@ docker run --name cont2 program
 docker images
 docker image tag "   " monishavp25/program
 docker push monishavp25/program:latest
-    
+--------------------------------------------------------------------------------------
 
+cd "DevOps"
+git clone "------"
+cd DockerProgram
+git add
+git commit -m "Commit"
+git push
+docker build -t program .
+docker run --name cont1 program
+docker run --name cont2 program
+docker images
+docker image tag "   " monishavp25/program
+docker push monishavp25/program:latest   -------------------> gitbash
 
-    
+in jenkins
+
+Cloudbee docker custom build environment
+docker
+docker pipelines
+docker compose
+
+new item in pipeline
+script
+
+pipeline {
+    agent any
+
+    environment {
+        registry = "monishavp25/test17:latest"
+        registryCredential = "jenkin_docker_token"
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scmGit(
+                    branches: [[name: '*/main']],
+                    extensions: [],
+                    userRemoteConfigs: [[
+                        url: 'https://github.com/Monishavp25/docker.git'
+                    ]]
+                )
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    dockerImage = docker.build(registry)
+                }
+            }
+        }
+    }
+}
+     
+        
+freestyle
+java --version
+https://my.slack.com/services/new/jenkins-ci
+build start 
+success    
+failure
